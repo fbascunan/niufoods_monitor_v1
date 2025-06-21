4399fbc49f485c42aa229207eab4233a3dcbc397
FelipeAndresBascunanMorales <f.bascunan03@gmail.com>
2025-06-21 02:08:53 -0400

    feat: adding restaurant details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "restaurants#index"
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :show]
   resources :devices, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
